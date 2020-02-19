a79b072029e3ff987c247dfde46e36a26509a968
Bakedbow <bow2dragon@gmail.com>
2020-02-18 21:10:37 -0800

    k

--- php/index.php
+++ php/index.php
@@ -4,7 +4,7 @@ header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
 header('Access-Control-Allow-Credentials: true');
 ?>
-
+ 
 <?php
 include('db.php');
 include('functions.php');
